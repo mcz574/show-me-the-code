1b2c7e85ba2dd95982ded1036623c2f82aa70ed6
mcz574 <574388073@qq.com>
2019-07-12 01:24:19 +0800

    完成0001生成200个优惠码题目

--- 0001/0001.py
+++ 0001/0001.py
@@ -1,11 +1,12 @@
 import random
 import string
-
+#优惠码格式为随机字母加数字的组合
 selectWord = string.ascii_letters + "0123456789"
+#创建优惠码列表
 codes = []
-
-def code_gen(count, length):
-    while len(codes) < count:
+# num优惠码数量，length优惠码长度
+def code_gen(num, length):
+    while len(codes) < num:
         code = ""
         for y in range(length):
             code += random.choice(selectWord)
@@ -13,5 +14,5 @@ def code_gen(count, length):
             codes.append(code)
 
 if __name__ == '__main__':
-    code_gen(10, 20)
+    code_gen(200, 20) #200个长度为20的优惠码
     print(codes)
